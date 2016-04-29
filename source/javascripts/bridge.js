5bcde7e13afe01d26b01b36a27a4ba58afbe1fa6
tshiwakoti <tshiwakoti@outlook.com>
2016-04-29 00:52:10 -0700

    Initial push

---
+++ source/javascripts/bridge.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require wow.min
+//= require bootstrap
+//= require creative
+//= require classie
+//= require cbpAnimatedHeader
+//= require jquery.easing.min
+//= require jquery.fittext
